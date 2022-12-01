76df8fc7f50ffe0debd49504d20842c1e78d8295
Dak Lod <trandacloc123@gmail.com>
2022-12-01 16:33:33 +0700

    4:33 1/12/2022

--- QuanLyVanPhongPham/src/ManagerProduct.java
+++ QuanLyVanPhongPham/src/ManagerProduct.java
@@ -9,6 +9,52 @@ public class ManagerProduct extends Manager{
 
     private String file_url = "DSSP.txt";
     
+    public void main(String[] args) {
+        while (true) {
+            MenuSelect menu = new MenuSelect(new String[] {
+                "Danh sách sản phẩm",
+                "Thêm sản phẩm",
+                "Tìm kiếm sản phẩm",
+                "Sửa sản phẩm",
+                "Xoá sản phẩm",
+                "Đọc",
+                "Ghi",
+                "Quay lại",
+                "Thoát"
+            });
+            int select =  menu.showMenu();
+            if (getMethod(select) == -1) return;
+            
+        }
+    }
+    
+    @Override
+    public void show() {
+        show(productList);
+    }
+
+    public void show(Product[] prd) {
+        if (prd[0] == null) {
+            System.out.println("Danh sách trống!");
+            return;
+        }
+        System.out.printf("\t\t\t           %-21s           \t\t\t\n", "DANH SÁCH SẢN PHẨM");
+        for (int i = 0; i < 92; i++)
+            System.out.print("=");
+        System.out.println();
+        System.out.printf("|%-7s|%-7s|%-29s|%-14s|%-29s|\n", "STT", "ID", "Tên sản phẩm", "Đơn giá", "Mô tả");
+        for (int i = 0; i < 92; i++)
+            System.out.print("-");
+        System.out.println();
+        for (int i = 0; i < prd.length; i++){
+            if (prd[i] == null) break;
+            System.out.printf("|%-7s|%-7s|%-29s|%-14s|%-29s|\n", (i + 1),prd[i].getProductId(),prd[i].getProductName(), prd[i].getPrice(), prd[i].getDes());
+        }
+        for (int i = 0; i < 92; i++)
+            System.out.print("=");
+        System.out.println();
+    }
+
     @Override
     public void add(){
         String[] info = new MenuInput(new String[] {
@@ -34,96 +80,6 @@ public class ManagerProduct extends Manager{
         idCount++;
         productList[idCount - 1] = prd;
     }
-
-    public Product[] getProductByPrice(String min, String max){
-        if (min.trim() == "")
-            min = "0";
-        if (max.trim() == "")
-            max = "99999999999999999999999";
-        return getProductByPrice(Float.parseFloat(min), Float.parseFloat(max));
-    }
-    
-    public Product[] getProductByPrice(float min, float max){
-        Product[] result = new Product[100];
-        int index = -1;
-        for (int i = 0; i < idCount; i++) {
-            if (productList[i] == null) continue;
-            if (productList[i].getPrice() >= min && productList[i].getPrice() <= max){
-                index++;
-                result[index] = productList[i];
-            }
-        }  
-        if (index > -1){
-            System.out.println("Đã tìm thấy " + (index + 1) + " sản phẩm");
-            // for (Product item : result){
-            //     if (item == null) break;
-            //     item.printProduct();
-            // }
-            this.show(result);
-        }else {
-            System.out.println("Không tìm thấy sản phẩm!");
-            return null;
-        }
-        return result;
-    }
-
-    public Product[] getProductByName(String name){
-        Product[] result = new Product[100];
-        int index = -1;
-        for (int i = 0; i < idCount; i++) {
-            if (productList[i] == null) continue;
-            if (productList[i].getProductName().toLowerCase().indexOf(name.toLowerCase()) > -1 || 
-            productList[i].getProductName().toLowerCase().indexOf(Main.removeAccents(name.toLowerCase())) > -1){
-                index ++;
-                result[index] = productList[i];
-            }
-        }  
-        if (index > -1){
-            System.out.println("Đã tìm thấy " + (index + 1) + " sản phẩm");
-            // for (Product item : result){
-            //     if (item == null) break;
-            //     item.printProduct();
-            // }
-            this.show(result);
-        }else {
-            System.out.println("Không tìm thấy sản phẩm!");
-            return null;
-        }
-        return result;
-    }
-
-    public Product getProductById(String id){
-        for (int i = 0; i < idCount; i++) {
-            if (productList[i] == null) continue;
-            if (productList[i].getProductId().equals(id.trim())) 
-                return productList[i];
-        }
-        // System.out.println("Không tìm thấy sản phẩm!");
-        return null;
-    }
-    
-
-
-    public void main(String[] args) {
-        while (true) {
-            MenuSelect menu = new MenuSelect(new String[] {
-                "Danh sách sản phẩm",
-                "Thêm sản phẩm",
-                "Tìm kiếm sản phẩm",
-                "Sửa sản phẩm",
-                "Xoá sản phẩm",
-                "Đọc",
-                "Ghi",
-                "Quay lại",
-                "Thoát"
-            });
-            int select =  menu.showMenu();
-            if (getMethod(select) == -1) return;
-
-        }
-    }
-
-
 
     @Override
     public void edit() {
@@ -155,7 +111,7 @@ public class ManagerProduct extends Manager{
                             infoChange[0] = new MenuInput(new String[] {
                                 "Mã cũ: " + productList[i].getProductId() + "\nMã mới"
                             });
-            
+                            
                             infoChange[1] = new MenuInput(new String[] {
                                 "Tên cũ: " + productList[i].getProductName() + "\nTên mới"
                             });
@@ -232,7 +188,6 @@ public class ManagerProduct extends Manager{
 
     }
 
-
     @Override
     public void search() {
         if (productCount == 0){
@@ -282,35 +237,28 @@ public class ManagerProduct extends Manager{
         }
     }   
 
-    
-
-
     @Override
-    public void show() {
-        show(productList);
+    public void remove() {
+        String[] info = new MenuInput(new String[] {
+            "Nhập mã sản phẩm cần xoá"
+        }).showMenu();
+
+        if (info != null)
+            for (int i = 0; i < idCount; i++) {
+                if (productList[i] == null) continue;
+                if (productList[i].getProductId().equals(info[0].trim())) {
+                    productCount--;
+                    System.out.println("Đã xoá sản phẩm " + productList[i].getProductName());
+                    for (int x = i; x < idCount - 1; x ++){
+                        productList[x] = productList[x + 1];
+                    }
+                    productList[idCount - 1] = null;
+                    return;
+                }
+            }
+        
     }
 
-    public void show(Product[] prd) {
-        if (prd[0] == null) {
-            System.out.println("Danh sách trống!");
-            return;
-        }
-        System.out.printf("\t\t\t           %-21s           \t\t\t\n", "DANH SÁCH SẢN PHẨM");
-        for (int i = 0; i < 92; i++)
-            System.out.print("=");
-        System.out.println();
-        System.out.printf("|%-7s|%-7s|%-29s|%-14s|%-29s|\n", "STT", "ID", "Tên sản phẩm", "Đơn giá", "Mô tả");
-        for (int i = 0; i < 92; i++)
-            System.out.print("-");
-        System.out.println();
-        for (int i = 0; i < prd.length; i++){
-            if (prd[i] == null) break;
-            System.out.printf("|%-7s|%-7s|%-29s|%-14s|%-29s|\n", (i + 1),prd[i].getProductId(),prd[i].getProductName(), prd[i].getPrice(), prd[i].getDes());
-        }
-        for (int i = 0; i < 92; i++)
-            System.out.print("=");
-        System.out.println();
-    }
 
     public void read() {
         String[] data = read(file_url);
@@ -333,27 +281,90 @@ public class ManagerProduct extends Manager{
     }
 
 
-    @Override
-    public void remove() {
-        String[] info = new MenuInput(new String[] {
-            "Nhập mã sản phẩm cần xoá"
-        }).showMenu();
-
-        if (info != null)
-            for (int i = 0; i < idCount; i++) {
-                if (productList[i] == null) continue;
-                if (productList[i].getProductId().equals(info[0].trim())) {
-                    productCount--;
-                    System.out.println("Đã xoá sản phẩm " + productList[i].getProductName());
-                    for (int x = i; x < idCount - 1; x ++){
-                        productList[x] = productList[x + 1];
-                    }
-                    productList[idCount - 1] = null;
-                    return;
-                }
-            }
-        
+    public Product[] getProductByPrice(String min, String max){
+        if (min.trim() == "")
+            min = "0";
+        if (max.trim() == "")
+            max = "99999999999999999999999";
+        return getProductByPrice(Float.parseFloat(min), Float.parseFloat(max));
     }
+    
+    public Product[] getProductByPrice(float min, float max){
+        Product[] result = new Product[100];
+        int index = -1;
+        for (int i = 0; i < idCount; i++) {
+            if (productList[i] == null) continue;
+            if (productList[i].getPrice() >= min && productList[i].getPrice() <= max){
+                index++;
+                result[index] = productList[i];
+            }
+        }  
+        if (index > -1){
+            System.out.println("Đã tìm thấy " + (index + 1) + " sản phẩm");
+            // for (Product item : result){
+            //     if (item == null) break;
+            //     item.printProduct();
+            // }
+            this.show(result);
+        }else {
+            System.out.println("Không tìm thấy sản phẩm!");
+            return null;
+        }
+        return result;
+    }
+
+    public Product[] getProductByName(String name){
+        Product[] result = new Product[100];
+        int index = -1;
+        for (int i = 0; i < idCount; i++) {
+            if (productList[i] == null) continue;
+            if (productList[i].getProductName().toLowerCase().indexOf(name.toLowerCase()) > -1 || 
+            productList[i].getProductName().toLowerCase().indexOf(Main.removeAccents(name.toLowerCase())) > -1){
+                index ++;
+                result[index] = productList[i];
+            }
+        }  
+        if (index > -1){
+            System.out.println("Đã tìm thấy " + (index + 1) + " sản phẩm");
+            // for (Product item : result){
+            //     if (item == null) break;
+            //     item.printProduct();
+            // }
+            this.show(result);
+        }else {
+            System.out.println("Không tìm thấy sản phẩm!");
+            return null;
+        }
+        return result;
+    }
+
+    public Product getProductById(String id){
+        for (int i = 0; i < idCount; i++) {
+            if (productList[i] == null) continue;
+            if (productList[i].getProductId().equals(id.trim())) 
+                return productList[i];
+        }
+        // System.out.println("Không tìm thấy sản phẩm!");
+        return null;
+    }
+    
+
+
+    
+    
+
+    
+
+
+    
+
+    
+
+
+
+    
+
+    
 
     public Product[] getProductList() {
         return productList;
